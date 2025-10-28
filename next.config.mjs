6ab0a7baa9152689304c7c1cc9f7fbc991c7b39a
jenji-12 <najen2546@gmail.com>
2025-10-29 00:11:20 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-// next.config.mjs
+import path from 'path'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  webpack(config) {
+    // ย้ำ alias @ ให้ชี้ root เสมอ
+    config.resolve.alias['@'] = path.resolve(process.cwd())
+    return config
+  },
 }
-export default nextConfig
