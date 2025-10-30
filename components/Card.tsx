e1a38413c24687a7a7df911a2c882fbaa9990d31
jenji-12 <najen2546@gmail.com>
2025-10-30 16:29:45 +0700

    Create Card.tsx

---
+++ components/Card.tsx
@@ -0,0 +1,24 @@
+// components/Card.tsx
+import * as React from "react";
+
+type BaseProps = React.HTMLAttributes<HTMLDivElement> & { className?: string };
+
+export function Card({ className = "", ...props }: BaseProps) {
+  return <div className={`rounded-2xl border p-4 shadow-sm ${className}`} {...props} />;
+}
+
+export function CardHeader({ className = "", ...props }: BaseProps) {
+  return <div className={`mb-3 ${className}`} {...props} />;
+}
+
+export function CardTitle({ className = "", ...props }: BaseProps) {
+  return <h3 className={`text-lg font-semibold ${className}`} {...props} />;
+}
+
+export function CardContent({ className = "", ...props }: BaseProps) {
+  return <div className={`${className}`} {...props} />;
+}
+
+export function CardFooter({ className = "", ...props }: BaseProps) {
+  return <div className={`mt-3 ${className}`} {...props} />;
+}
